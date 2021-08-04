0bdc9fdaf552fec16d2f5564b4310a9061ff2958
Francois van Rensburg <jvrensburgfrans@gmail.com>
2021-08-04 14:25:44 +0200

    Added form components and code snipit

--- Demo/src/components/Forms.tsx
+++ Demo/src/components/Forms.tsx
@@ -1,9 +1,16 @@
-import { InfoButton, Input, Switch } from "../../../src";
+import { DatePicker, InfoButton, Input, PageHeading, SectionHeading, Switch } from "../../../src";
 import { useState } from "react";
 
 function Forms() {
-  const [inlineInput, setInlineInput] = useState<boolean>(false);
+  const [inlineInput, setInlineInput] = useState<boolean>(true);
   const [selection, setSelection] = useState<any>(null);
+
+  const [fromDate, setFromDate] = useState<any>();
+  const [toDate, setToDate] = useState<any>();
+
+  // display code blocks
+  const [showDatePicker, setShowDatePicker] = useState<boolean>(false);
+  const [showInputInterface, setShowInputInterface] = useState<boolean>(false);
 
   // Render methods
   const InputPrice = () => (
@@ -46,10 +53,109 @@ function Forms() {
     </div>
   );
 
+  const datePickerInput = (val: any, setVal: any, label: string) => {
+    return (
+      <DatePicker
+        label={label}
+        labelInline
+        placeholder={`${label} Date`}
+        dateFormat={"yyyy-MM-DD"}
+        selected={val}
+        onChange={(date: any) => {
+          setVal(date);
+        }}
+      />
+    );
+  };
+
   return (
     <div className="mt-5">
+      <PageHeading>Form Components</PageHeading>
+      <SectionHeading>Input and checkbox components</SectionHeading>
+      <hr />
+      <Switch
+        checked={showInputInterface}
+        onChange={() => setShowInputInterface(!showInputInterface)}
+      />
+      {showInputInterface && (
+        <div style={{ width: "40%" }}>
+          <pre>
+            <code>{`
+        interface IInputProps {
+          label?: string;
+          labelInline?: boolean;
+          labelClassName?: string;
+          htmlFor?: string;
+          register?: any;
+          name?: string;
+          defaultValue?: any;
+          value?: any;
+          validationError?: any;
+          type?: string;
+          onChange?: any;
+          onFocus?: any;
+          onBlur?: any;
+          onKeyPress?: any;
+          onKeyUp?: any;
+          step?: number;
+          min?: number;
+          max?: number;
+          autoComplete?: any;
+          disabled?: boolean;
+          reference?: any;
+          placeholder?: string;
+          id?: string;
+          containerClassName?: string;
+          errorMessage?: string;
+          autoFocus?: any;
+          optional?: boolean;
+          readOnly?: boolean;
+
+          info?: any;
+          inputFieldId?: string;
+          appendIcon?: IconProp;
+          appendIconId?: string;
+          appendText?: string;
+          appendSelectProps?: any;
+          prependText?: string;
+          inputFieldStyle?: any;
+          inputId?: string;
+        }`}</code>
+          </pre>
+        </div>
+      )}
       <InputPrice />
+
       <SelectInput />
+      <SectionHeading>Date picker</SectionHeading>
+      <hr />
+      <div className="mt-5">
+        <Switch
+          checked={showDatePicker}
+          onChange={() => setShowDatePicker(!showDatePicker)}
+          label="Show date picker code"
+        />
+      </div>
+      {showDatePicker && (
+        <pre>
+          <code>{`
+            <DatePicker
+              label={"date"}
+              labelInline
+              placeholder={"Date"}
+              dateFormat={"yyyy-MM-DD"}
+              selected={"date"}
+              onChange={"(date: any) => {
+                "setDate(date)";
+            }}
+          />`}</code>
+        </pre>
+      )}
+
+      <div className="flex gap-10 mt-5">
+        {datePickerInput(fromDate, setFromDate, "From")}
+        {datePickerInput(toDate, setToDate, "To")}
+      </div>
     </div>
   );
 }
--- Demo/src/components/Modals.tsx
+++ Demo/src/components/Modals.tsx
@@ -1,76 +1,129 @@
 import { Button, Modal, PageActionsPanel } from "../../../src";
 import { useState } from "react";
-import ReactJson from "react-json-view";
-
-import ModalInterface from '../interfaces/modal.interface.json'
 
 function Modals() {
-  const [smallModal, setSmallModal] = useState<boolean>(false)
-  const [mediumModal, setMediumModal] = useState<boolean>(false)
-  const [largeModal, setLargeModal] = useState<boolean>(false)
-  const [loading, setLoading] = useState<boolean>(false)
+  const [smallModal, setSmallModal] = useState<boolean>(false);
+  const [mediumModal, setMediumModal] = useState<boolean>(false);
+  const [largeModal, setLargeModal] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   // loader
   function onCloseModal(time: number, size: any) {
-    setLoading(true)
-    setTimeout(()=>{
-      size(false)
-      setLoading(false)
-    }, time)
+    setLoading(true);
+    setTimeout(() => {
+      size(false);
+      setLoading(false);
+    }, time);
   }
   return (
     <div className="mt-5">
       <PageActionsPanel title="Modals page">
-        {[{title: "Small modal", size: setSmallModal }, {title: "Medium modal", size: setMediumModal }, {title: "Large modal", size: setLargeModal }].map((size: any) => (
-          <Button.Primary key={size.title} title={`${size.title}`} onClick={()=>size.size(true)}/>
+        {[
+          { title: "Small modal", size: setSmallModal },
+          { title: "Medium modal", size: setMediumModal },
+          { title: "Large modal", size: setLargeModal }
+        ].map((size: any) => (
+          <Button.Primary
+            key={size.title}
+            title={`${size.title}`}
+            onClick={() => size.size(true)}
+          />
         ))}
       </PageActionsPanel>
-      <Modal.Medium show={mediumModal} closeButton onHide={()=>setMediumModal(false)} title="Medium modal" disableClickOutsideToClose>
+      <Modal.Medium
+        show={mediumModal}
+        closeButton
+        onHide={() => setMediumModal(false)}
+        title="Medium modal"
+        disableClickOutsideToClose
+      >
         <div>
-          <ReactJson src={ModalInterface} collapsed={false} displayDataTypes={false} />
-          <p>View the{" "}
-            <a style={{color: "blue"}} href="https://github.com/uafrica/ui-framework/blob/main/src/Modal.tsx" target="_blank">Modal</a>{" "}
-            here
-          </p>
+          <pre>
+            <code>{`
+            interface ISmallMediumModalProps {
+                show: boolean;
+                children: any;
+                onHide?: any;
+                title?: any;
+                icon?: IconProp;
+                closeButton: boolean;
+                disableClickOutsideToClose?: boolean;
+              }
+              
+            interface IBaseProps extends ISmallMediumModalProps {
+                className?: string;
+              }
+            `}</code>
+          </pre>
         </div>
         <Modal.ButtonsPanel>
-          <Button.Cancel title="Cancel" onClick={()=>setMediumModal(false)} />
-          <Button.Primary title="Save" loadingTitle="Saving" isLoading={loading} onClick={()=>onCloseModal(1500, setMediumModal)} />
+          <Button.Cancel title="Cancel" onClick={() => setMediumModal(false)} />
+          <Button.Primary
+            title="Save"
+            loadingTitle="Saving"
+            isLoading={loading}
+            onClick={() => onCloseModal(1500, setMediumModal)}
+          />
         </Modal.ButtonsPanel>
       </Modal.Medium>
-      <Modal.Small show={smallModal} closeButton={false} onHide={()=>setSmallModal(false)} title="Small modal">
-        <div>
-          <div>
-            <ReactJson src={ModalInterface} collapsed={false} displayDataTypes={false} />
-            <p>View the{" "}
-              <a style={{color: "blue"}} href="https://github.com/uafrica/ui-framework/blob/main/src/Modal.tsx" target="_blank">Modal</a>{" "}
-              here
-            </p>
-          </div>
-        </div>
+      <Modal.Small
+        show={smallModal}
+        closeButton={false}
+        onHide={() => setSmallModal(false)}
+        title="Small modal"
+      >
+        <div>Modal body goes here</div>
         <Modal.ButtonsPanel>
-          <Button.Cancel title="Cancel" onClick={()=>setSmallModal(false)} />
-          <Button.Primary title="Save" loadingTitle="Saving" isLoading={loading} onClick={()=>onCloseModal(1500, setSmallModal)} />
+          <Button.Cancel title="Cancel" onClick={() => setSmallModal(false)} />
+          <Button.Primary
+            title="Save"
+            loadingTitle="Saving"
+            isLoading={loading}
+            onClick={() => onCloseModal(1500, setSmallModal)}
+          />
         </Modal.ButtonsPanel>
       </Modal.Small>
-      <Modal.Large show={largeModal} closeButton onHide={()=>setLargeModal(false)} title="Large modal">
+      <Modal.Large
+        show={largeModal}
+        closeButton
+        onHide={() => setLargeModal(false)}
+        title="Large modal"
+      >
         <div>
-          <div>
-            <ReactJson src={ModalInterface} collapsed={false} displayDataTypes={false} />
-            <p>View the{" "}
-              <a style={{color: "blue"}} href="https://github.com/uafrica/ui-framework/blob/main/src/Modal.tsx" target="_blank">Modal</a>{" "}
-              here
-            </p>
-          </div>
+          <pre>
+            <code>{`
+              <Modal.Large
+                show={largeModal}
+                closeButton
+                onHide={() => setLargeModal(false)}
+                title="Large modal"
+              >
+              <div>Modal body goes here</div>
+              <Modal.ButtonsPanel>
+                <Button.Cancel title="Cancel" onClick={() => setLargeModal(false)} />
+                <Button.Primary
+                  title="Save"
+                  loadingTitle="Saving"
+                  isLoading={loading}
+                  onClick={() => onCloseModal(1500, setLargeModal)}
+                />
+              </Modal.ButtonsPanel>
+            </Modal.Large>
+        `}</code>
+          </pre>
         </div>
         <Modal.ButtonsPanel>
-          <Button.Cancel title="Cancel" onClick={()=>setLargeModal(false)} />
-          <Button.Primary title="Save" loadingTitle="Saving" isLoading={loading} onClick={()=>onCloseModal(1500, setLargeModal)} />
+          <Button.Cancel title="Cancel" onClick={() => setLargeModal(false)} />
+          <Button.Primary
+            title="Save"
+            loadingTitle="Saving"
+            isLoading={loading}
+            onClick={() => onCloseModal(1500, setLargeModal)}
+          />
         </Modal.ButtonsPanel>
       </Modal.Large>
     </div>
-
-  )
+  );
 }
 
-export default Modals
+export default Modals;
