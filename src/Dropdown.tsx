36e5806b077b999eb02ae5de94468f33319f1286
Melissa <melissa@uafrica.com>
2021-08-05 13:52:34 +0200

    added menu heading to dropdown

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -21,6 +21,11 @@ interface IMenuItem {
   onClick: any;
   id?: string;
 }
+interface IMenuHeading {
+  title: string;
+  icon?: IconProp;
+  id?: string;
+}
 
 // Implementation
 function DropdownMenu(props: IDropdown) {
@@ -136,9 +141,36 @@ function MenuItem(props: IMenuItem) {
     </div>
   );
 }
+function MenuHeading(props: IMenuHeading) {
+  let { title, icon, id } = props;
+
+  return (
+    <div id={id}>
+      <Menu.Item>
+        {() => (
+          <div
+            className={
+              "group flex items-center px-4 py-2 cursor-pointer font-semibold text-gray-700"
+            }
+          >
+            {icon && (
+              <FontAwesomeIcon
+                icon={icon}
+                className="mr-3 h-5 w-5 text-gray-500 group-hover:text-gray-900"
+                aria-hidden="true"
+              />
+            )}
+            {title}
+          </div>
+        )}
+      </Menu.Item>
+    </div>
+  );
+}
 
 const Dropdown = {
   MenuItem,
+  MenuHeading,
   Menu: DropdownMenu,
   ContextMenu
 };
