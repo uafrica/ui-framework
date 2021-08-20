409aa4f8306adf9d1568001d9671fbcb44e30fe7
Melissa <melissa@uafrica.com>
2021-08-20 11:41:30 +0200

    saving overlay now works with portal in order to display correctly when used on modals

--- src/SavingOverlay.tsx
+++ src/SavingOverlay.tsx
@@ -1,3 +1,20 @@
+import { createPortal } from "react-dom";
+import { useEffect, useState, ReactNode } from "react";
+
+function Portal(props: { children: ReactNode }) {
+  let { children } = props;
+  let [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+  return createPortal(children, document.body);
+}
+
 export function SavingOverlay() {
-  return <div className="saving-overlay" onClick={e => e.stopPropagation()} />;
+  return (
+    <Portal>
+      <div className="saving-overlay" onClick={e => e.stopPropagation()} />
+    </Portal>
+  );
 }
