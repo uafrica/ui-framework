448c38f5414c30d16fc55611baab92a1b45fc2a9
Cornelius Rautenbach <cornel.rautenbach@gmail.com>
2021-10-05 10:38:56 +0200

    1.1.79 - icon size

--- src/PageHeading.tsx
+++ src/PageHeading.tsx
@@ -13,7 +13,7 @@ function PageHeading(props: IPageHeading) {
   return (
     <div className={center ? " mx-auto text-center items-center " : ""} id={id}>
       <h1 className="text-xl font-bold text-gray-900 uppercase">
-        {icon && <FontAwesomeIcon icon={icon} className={"mr-3"} size="4x" />}
+        {icon && <FontAwesomeIcon icon={icon} className={"mr-3"} size="sm" />}
         {children}
       </h1>
     </div>
