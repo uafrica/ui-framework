40a63c5104128ad07cbe91b563f3ee53683152ca
Cornelius Rautenbach <cornel.rautenbach@gmail.com>
2021-11-23 15:43:42 +0200

    1.2.11 - Add infobutton to labelwithvalue

--- src/Label.tsx
+++ src/Label.tsx
@@ -1,3 +1,5 @@
+import { InfoButton } from "InfoButton";
+
 // Interface
 interface ILabel {
   children?: any;
@@ -10,6 +12,7 @@ interface ILabelWithValue {
   label: string;
   value: any;
   noMargin?: boolean;
+  info?: string;
 }
 
 // Implementation
@@ -27,12 +30,13 @@ function Label(props: ILabel) {
 }
 
 function LabelWithValue(props: ILabelWithValue) {
-  let { label, value, noMargin } = props;
+  let { label, value, noMargin, info } = props;
 
   return (
     <div className={"flex flex-row space-x-2 flex-wrap " + (noMargin ? "" : " pt-2")}>
       <label className={"u-label-text self-baseline " + (noMargin ? "" : " mb-2 ")}>{label}</label>
       <div className="text-left">{value}</div>
+      {info && <InfoButton>{info}</InfoButton>}
     </div>
   );
 }
