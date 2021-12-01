324a443805ae74278f44caedc12a3c26b737c17e
Francois van Rensburg <jvrensburgfrans@gmail.com>
2021-12-01 13:54:41 +0200

    Added hooks for usePrevious and useTabs

---
+++ src/hooks/useTabs.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import usePrevious from "hooks/usePrevious";
+import { useHistory } from "react-router-dom";
+
+function useTabs(defaultTab: string) {
+  const history: any = useHistory();
+  const [activeTabID, setActiveTabID] = useState<any>();
+
+  const prevLocation = usePrevious(location.search);
+
+  useEffect(() => {
+    if (!activeTabID || activeTabID === "") {
+      return setActiveTabID(defaultTab);
+    }
+    setActiveTabID(activeTabID);
+  }, [activeTabID]);
+
+  useEffect(() => {
+    if (location.search !== prevLocation) {
+      const urlParams = new URLSearchParams(location.search);
+      let params = urlParams.get("tab");
+      setActiveTabID(params);
+    }
+  });
+
+  function onTabSelected(tab: any) {
+    const queryParams = new URLSearchParams(location.search);
+    queryParams.set("tab", tab);
+
+    history.push({
+      pathname: location.pathname,
+      search: queryParams.toString()
+    });
+  }
+
+  return { activeTabID, onTabSelected };
+}
+
+export default useTabs;
