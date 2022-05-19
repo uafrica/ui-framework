55f9c2ab1d52239cb4350fe8b4b94914202a1b19
Francois van Rensburg <jvrensburgfrans@gmail.com>
2022-05-19 12:53:42 +0200

    Created on click outside event.

---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+export default function useOnClickOutside(
+  ref: React.MutableRefObject<HTMLElement | undefined>,
+  handler: Function
+) {
+  useEffect(() => {
+    const listener = (e: any) => {
+      if (!ref.current || ref.current.contains(e.target)) {
+        return;
+      }
+      handler(e);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
