24a95eeb77ffd88cbe29f81d2db60b216c043ccf
Francois van Rensburg <jvrensburgfrans@gmail.com>
2022-12-05 15:06:46 +0200

    Export IRole interface with the other interfaces

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,4 +1,5 @@
 import { INavItem } from "./navitem.interface";
 import { IGenericUser } from "./genericUser.interface";
+import {IRole} from "./role.intarface";
 
-export type { INavItem, IGenericUser };
+export type { INavItem, IGenericUser, IRole };
