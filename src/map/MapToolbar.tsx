6d4b2a635fa9fe5ce94b423fd841d28f4c8dce83
Melissa <melissa@uafrica.com>
2023-03-13 12:40:24 +0200

    version @1.2.227

--- src/map/MapToolbar.tsx
+++ src/map/MapToolbar.tsx
@@ -12,30 +12,36 @@ function MapToolbar(props: {
 
   if (!editMode) return null;
 
+  let snapIcon: any = doSnap ? "compress-arrows-alt" : "location-crosshairs";
+
   return (
     <div className="flex flex-row space-x-4">
       <Button.Tertiary
         hoverTitle="Select tool"
         icon="mouse-pointer"
         id="mouse_pointer"
-        className={editMode === "select" ? "ring-2 ring-blue" : ""}
-        onClick={props.onSelectModeClicked}
+        className={editMode === "select" ? "ring-2 ring-primary" : ""}
+        onClick={(e: any) => {
+          props.onSelectModeClicked(e);
+        }}
       />
 
       <Button.Tertiary
         hoverTitle="Draw tool"
         icon="draw-polygon"
         id="draw_tool"
-        className={editMode === "draw" ? "ring-2 ring-blue" : ""}
-        onClick={props.onDrawModeClicked}
+        className={editMode === "draw" ? "ring-2 ring-primary" : ""}
+        onClick={(e: any) => {
+          props.onDrawModeClicked(e);
+        }}
       />
 
       <Button.Tertiary
         hoverTitle={doSnap ? "Disable polygon point snap" : "Enable polygon point snap"}
-        icon="compress-arrows-alt"
-        className={doSnap ? "ring-2 ring-blue" : ""}
-        onClick={() => {
-          props.onSnapToggle();
+        icon={snapIcon}
+        className={doSnap ? "ring-2 ring-primary" : ""}
+        onClick={(e: any) => {
+          props.onSnapToggle(e);
         }}
       />
 
