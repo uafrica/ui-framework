13cb4968b5f2a11dd84c964ad5d59c12ace2cea5
Cornel Rautenbach <corneliusr>
2023-04-24 10:44:52 +0200

    1.2.250 - Fix lowercase "Bob box"

--- src/utils/generalUtils.tsx
+++ src/utils/generalUtils.tsx
@@ -247,7 +247,14 @@ function keyToHumanReadable(key: string | undefined): string {
   // camelcase to sentence case
   keyHumanReadable = keyHumanReadable.replace(/([A-Z])/g, " $1").trim();
 
-  return keyHumanReadable.charAt(0).toUpperCase() + keyHumanReadable.slice(1).toLowerCase();
+  let sentenceCaseKey =
+    keyHumanReadable.charAt(0).toUpperCase() + keyHumanReadable.slice(1).toLowerCase();
+
+  sentenceCaseKey = replaceAll(keyHumanReadable, "Bob box", "Bob Box");
+  sentenceCaseKey = replaceAll(keyHumanReadable, "Bob pay", "Bob Pay");
+  sentenceCaseKey = replaceAll(keyHumanReadable, "Bob go", "Bob Go");
+
+  return sentenceCaseKey;
 }
 
 function cleanURL(url: string): string {
