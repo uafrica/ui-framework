956361d4598bf46a57d38478eedb1975c1f3b0f8
Lehlohonolo <lehlohonolo@bob.co.za>
2023-10-24 13:36:39 +0200

    Added new component ExpiryDateInput - Used value prop to update the month and year values instead of the states

--- src/ExpiryDateInput.tsx
+++ src/ExpiryDateInput.tsx
@@ -1,41 +1,14 @@
 import { Label } from "./Label";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 
 interface IProps {
-  value?: any;
+  value: { month: string; year: string };
   onChange: Function;
 }
 function ExpiryDateInput(props: IProps) {
   let { value } = props;
-  let [expiryMonth, setExpiryMonth] = useState<string>();
-  let [expiryYear, setExpiryYear] = useState<string>();
-  let [expiryDate, setExpiryDate] = useState<any>({});
+
   const yearInputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (value) {
-      setExpiryMonth(value.month);
-      setExpiryYear(value.year);
-    }
-  }, []);
-
-  function handleDateChange(inputValue: any, type: string) {
-    value = null;
-    if (inputValue.length === 2) {
-      yearInputRef.current?.focus();
-    }
-    inputValue = inputValue.replace(/\D/g, "");
-    if (type == "month") {
-      setExpiryMonth(inputValue);
-      expiryDate.month = inputValue;
-    } else {
-      setExpiryYear(inputValue);
-      expiryDate.year = inputValue;
-    }
-
-    setExpiryDate({ ...expiryDate });
-    props.onChange({ ...expiryDate });
-  }
 
   /* --------------------------------*/
   /* RENDER METHODS */
@@ -50,11 +23,15 @@ function ExpiryDateInput(props: IProps) {
             type="text"
             placeholder="MM"
             className="w-8 py-1 border-none shadow-none focus:ring-0 px-0 "
-            value={expiryMonth}
+            value={value.month}
             maxLength={2}
             onChange={(e: any) => {
-              const inputValue = e.target.value;
-              handleDateChange(inputValue, "month");
+              let inputValue = e.target.value;
+              if (inputValue.length === 2) {
+                yearInputRef.current?.focus();
+              }
+              value.month = inputValue.replace(/\D/g, "");
+              props.onChange({ ...value });
             }}
           />
           <span className="text-gray-600 pl-1 pr-2 py-1">/</span>
@@ -62,11 +39,12 @@ function ExpiryDateInput(props: IProps) {
             type="text"
             placeholder="YY"
             className="w-8 py-1 border-none shadow-none focus:ring-0 px-0"
-            value={expiryYear}
+            value={value.year}
             maxLength={2}
             onChange={(e: any) => {
-              const inputValue = e.target.value;
-              handleDateChange(inputValue, "year");
+              let inputValue = e.target.value;
+              value.year = inputValue.replace(/\D/g, "");
+              props.onChange({ ...value });
             }}
             ref={yearInputRef}
           />
