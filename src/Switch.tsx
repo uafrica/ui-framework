e9072cf618e402dd27994e911e012e7fbbd6b02d
Melissa Greyling <melissa@bob.co.za>
2024-01-04 09:32:48 +0200

    Switch improvements

--- src/Switch.tsx
+++ src/Switch.tsx
@@ -4,6 +4,7 @@ import { InfoButton } from "./InfoButton";
 interface IProps {
   isChecked: boolean;
   isDisabled?: boolean;
+  isDoublePositive?: boolean;
   onChange: any;
   label?: string;
   info?: string;
@@ -11,7 +12,33 @@ interface IProps {
 }
 
 export default function Switch(props: IProps) {
-  let { isChecked, label, info, isDisabled, containerClassName } = props;
+  let { isChecked, label, info, isDisabled, containerClassName, isDoublePositive } = props;
+
+  function renderCheck() {
+    return (
+      <svg
+        className={`h-3 w-3 ${isDisabled ? "text-gray-600" : "text-primary-600"} `}
+        fill="currentColor"
+        viewBox="0 0 12 12"
+      >
+        <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+      </svg>
+    );
+  }
+
+  function renderX() {
+    return (
+      <svg className="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
+        <path
+          d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+          stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    );
+  }
 
   return (
     <div
@@ -31,7 +58,13 @@ export default function Switch(props: IProps) {
         className={
           " relative inline-flex flex-shrink-0 h-6.5 w-11 border-2 border-transparent rounded-full transition-colors ease-in-out duration-200  " +
           (isDisabled ? "pointer-events-none " : " hover:border-primary cursor-pointer ") +
-          (isChecked ? (isDisabled ? "bg-gray-400" : "bg-primary") : "bg-gray-200")
+          (isChecked
+            ? isDisabled
+              ? "bg-gray-400"
+              : "bg-primary"
+            : isDoublePositive
+            ? "bg-primary-light"
+            : "bg-gray-200")
         }
       >
         <span className="sr-only">Use setting</span>
@@ -48,15 +81,7 @@ export default function Switch(props: IProps) {
             }
             aria-hidden="true"
           >
-            <svg className="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
-              <path
-                d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            {isDoublePositive ? renderCheck() : renderX()}
           </span>
           <span
             className={
@@ -65,13 +90,7 @@ export default function Switch(props: IProps) {
             }
             aria-hidden="true"
           >
-            <svg
-              className={`h-3 w-3 ${isDisabled ? "text-gray-600" : "text-primary-600"} `}
-              fill="currentColor"
-              viewBox="0 0 12 12"
-            >
-              <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-            </svg>
+            {renderCheck()}
           </span>
         </span>
       </HeadlessSwitch>
