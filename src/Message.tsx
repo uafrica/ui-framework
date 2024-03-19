df79e8e2de6bf574411b4b327e6dcc7367c0aab4
Cornel Rautenbach <cornel.rautenbach@gmail.com>
2024-03-19 10:50:03 +0200

    2.0.62 Fix weird import

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,3 +1,5 @@
+// @ts-ignore
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { useState } from "react";
